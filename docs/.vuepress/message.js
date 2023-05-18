870440d5c21c8f6e2351089e7018f9dfab0f3aaa
reicalg2003 <2316723995@qq.com>
2023-05-18 19:36:51 +0800

    docs

---
+++ docs/.vuepress/message.js
@@ -0,0 +1,7 @@
+module.exports = {
+    //这里是将config.js中的公告部分单独提取出来，方便配置
+    message: [
+        '该博客主题为Aurora,<a href="https://github.com/qsyyke/vuepress-theme-aurora">vuepress-theme-Aurora</a>',
+        "慢慢写，慢慢更，一点也不着急",
+    ]
+}
